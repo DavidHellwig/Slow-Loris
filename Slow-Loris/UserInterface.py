2a4010fc4f72d78c890e5560ee08a1d15799b20e
David Hellwig <helldavi@isu.edu>
2022-09-27 21:10:49 -0600

    Small UI Changes

--- Slow-Loris/UserInterface.py
+++ Slow-Loris/UserInterface.py
@@ -11,36 +11,49 @@ class UserInterface:
 
     # Basic UI elements, need to redo this shit
     scene.title("Slow Loris Tool 4416")
+    #Screen Size
+    scene.geometry("1280x720")
 
-    scene.geometry("500x500")
+    #Menu
+    lorisMenu = tkinter.Menu(scene)
 
+
+    helpMenu = tkinter.Menu(lorisMenu,tearoff=0)
+    helpMenu.add_command(label="Help",command= 2*2)
+
+
+    #Attack Button
     attack = tkinter.Button(text="Start Attack", width=10, height=3, bg="snow4", fg="grey1")
 
     targetText = tkinter.Label(text="Target Address")
-
-    attackStatusContainer = tkinter.Frame(scene, width=499, highlightbackground="grey1", highlightthickness=3, bg="grey1", height=150)
-
-    liveFeed = tkinter.Text(attackStatusContainer, width=100, bg="grey1", fg="green", height=150)
+    #Frame to contain info that happens during attack
+    attackStatusContainer = tkinter.Frame(scene, width=50, highlightbackground="grey1", highlightthickness=1, bg="grey4", height=50)
+    #The live feed of the attack
+    liveFeed = tkinter.Text(attackStatusContainer, width=50, bg="grey1", fg="green", height=50)
 
     liveFeed.config(state="disabled")
 
 
     attackStatus = tkinter.Label(text="Attack Status")
-
+    #An entry field for getting the target of the attack
     target = tkinter.Entry(fg="grey1", bg="snow4", width=50)
 
 
-    attack.place(x=215, y=200)
+    attack.place(x=165, y=100)
 
-    target.place(x=100, y=190)
+    target.place(x=50, y=90)
 
-    targetText.place(x=100, y=170)
+    targetText.place(x=165, y=70)
 
-    attackStatus.place(x=215, y=275)
+    attackStatus.place(x=160, y=175)
 
-    attackStatusContainer.place(x=1, y=300)
+    attackStatusContainer.place(x=1, y=200)
 
     liveFeed.pack()
+
+    scene.config(menu=helpMenu)
+
+
 
 
 
@@ -48,6 +61,7 @@ class UserInterface:
 
     # Init method
     def init(self):
+
         self.scene.mainloop()
 
 
